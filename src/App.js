a8b7935029d0237ea5cc032ef3719b377037f638
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-04-28 01:18:02 -0300

    Diseño de la pagina New

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import List from './pages/list/List';
 import Single from './pages/single/Single';
 import New from './pages/new/New';
 
+import {userInputs, productInputs} from './formSource';
 
 import {
   BrowserRouter,
@@ -23,17 +24,12 @@ function App() {
             <Route path='users'>
               <Route index element={<List />}/>
               <Route path=':userId' element={<Single />}/>
-              <Route path='new' element={<New />}/>
-            </Route>
-            <Route path='users'>
-              <Route index element={<List />}/>
-              <Route path=':userId' element={<Single />}/>
-              <Route path='new' element={<New />}/>
+              <Route path='new' element={<New inputs={userInputs} title='Add New User'/>}/>
             </Route>
             <Route path='products'>
               <Route index element={<List />}/>
               <Route path=':productId' element={<Single />}/>
-              <Route path='new' element={<New />}/>
+              <Route path='new' element={<New inputs={productInputs} title='Add New Product'/>}/>
             </Route>
           </Route>
         </Routes>
